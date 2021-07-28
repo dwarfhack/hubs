b60a1e497688e0dcd47a39004bd88f580c85ba51
Marco Amann <marco.amann@digitalfrontiers.de>
2021-07-28 11:56:18 +0200

    stripped down on commented out code, moved tests to own test folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
-use std::{cell::UnsafeCell, fmt::Debug, sync::{Arc, atomic::{AtomicBool, AtomicUsize, Ordering}}};
+use std::{cell::UnsafeCell, fmt::Debug, sync::{Arc, atomic::{ AtomicUsize, Ordering}}};
 
+pub const HUBS_SIZE: usize = 4096;
+pub const CHUNK_SIZE: usize = 4096;
 
-
-
-
-const HUBS_SIZE: usize = 4096;
-const CHUNK_SIZE: usize = 4096;
-
-
+/**
+The Hubs data structure used for initialization.
+Access is possible through the xxx and xxx
+*/
 pub struct Hubs<T>{
     inner: HubsInner<T>
 }
 impl<T> Hubs<T> where T:Clone,T:Default, T:Debug{
-
+    /**
+    Take Ownership of the hubs and split it.
+    */
     pub fn split(self) -> (HubsProducer<T>, HubsConsumer<T>){
         let inner = Arc::new(self.inner);
         let tx = HubsProducer{
@@ -38,12 +39,10 @@ impl<T> Hubs<T> where T:Clone,T:Default, T:Debug{
                 read_ptr: AtomicUsize::new(0),
                 read_barrier: AtomicUsize::new(HUBS_SIZE-1),
                 write_ptr: AtomicUsize::new(0),
-                write_barrier: AtomicUsize::new(0),
-                // is_write_block_borrowed: AtomicBool::new(false)
+                write_barrier: AtomicUsize::new(0)
             } 
         }
     }
-
 }
 
 pub trait HubsInitializer{
@@ -199,13 +198,6 @@ enum ChunkBlockData<'a,T>  where T:Clone,T:Default, T:Debug{
 }
 
 
-
-// impl <'a,T> ChunkBlock<'a,T> where T:Clone,T:Default{
-//     pub fn return_to_hubs(&mut self){
-//         self.parent.return_chunk_block(&mut self);
-//     }
-// }
-
 impl <'a,T> Drop for ChunkBlock<'a,T>  where T:Clone,T:Default, T:Debug{
     fn drop(&mut self) {
         match self.parent{
@@ -231,10 +223,6 @@ impl <'a,T> HubsWriteAccess<'a,T> where T:Clone,T:Default, T:Debug{
 impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
     fn get_read_chunks_current(&self) -> ChunkBlock<T>{
 
-        // if self.is_write_block_borrowed.load(Ordering::SeqCst) {
-        //     return ChunkBlock::empty()
-        // }
-
         let read_end = self.write_barrier.load(Ordering::SeqCst);
         let read_start = self.read_ptr.load(Ordering::SeqCst);
 
@@ -256,9 +244,6 @@ impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
             ChunkBlockData::One(unsafe{ &(*self.chunks.get())[read_start..read_end] })
         };
 
-
-        // println!("Chunks: {:?}", chunks);
-        // self.is_write_block_borrowed.store(true, Ordering::SeqCst);
         ChunkBlock::new(chunks, &self)
     }
 
@@ -280,6 +265,11 @@ impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
         let next_write_pos = (write_pos + 1) % HUBS_SIZE;
 
         self.write_ptr.store( next_write_pos, Ordering::SeqCst);
+
+        /*
+         SAFETY:
+         
+         */
         let chunk = unsafe{ &mut(*self.chunks.get())[write_pos] };
         Some(HubsWriteAccess{
             chunk,
@@ -288,9 +278,6 @@ impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
     }
 
     fn return_chunk_block(&self, _block: &mut ChunkBlock<T>){
-        // if ! self.is_write_block_borrowed.load(Ordering::SeqCst) {
-        //     panic!("Tried to return block to hubs that has no block given out")
-        // }
 
         let read_end = self.read_barrier.load(Ordering::SeqCst);
         let mut read_ptr = self.read_ptr.load(Ordering::SeqCst);
@@ -301,9 +288,7 @@ impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
             panic!("Tried to return block to hubs that has no block given out")
         }
 
-        // let mut  read_ptr = self.read_ptr.load(Ordering::SeqCst);
         self.read_barrier.store(read_ptr, Ordering::SeqCst);
-        // self.is_write_block_borrowed.store(false,Ordering::SeqCst)
     }
 
     fn commit_chunk(&self, _write_access: HubsWriteAccess<T>){
@@ -314,334 +299,7 @@ impl<T> HubsInner<T> where T:Clone, T:Default, T:Debug{
             self.write_barrier.store(write_barrier, Ordering::SeqCst);
         }
         else  {
-            panic!("Nope, cannot do that")
+            panic!("Cant commit old chunk if alreadz borrowed new chunk")
         }
-    }
-}
-
-
-
-
-
-
-
-// unsafe cell
-// arc
-// tick insert/retrieve
-// non-overlapping elements based on tick?
-// preallocated slab like buffers
-// chunks of checkout/plcae back buffers
-
-
-
-#[cfg(test)]
-mod tests {
-
-    use std::{sync::mpsc, thread::{self, sleep}, time::Duration};
-
-    use crate::{HUBS_SIZE, HubsInitializer};
-
-    use super::Hubs;
-    
-    struct HubsInitializerU64{}
-    impl HubsInitializer for HubsInitializerU64{
-        type T = u64;
-
-        fn initialize_data(&self)-> Self::T {
-            0
-        }
-    }
-
-    #[test]
-    fn test_write_single() {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let chunk = tx.borrow_chunk_mut().unwrap();
-        chunk.chunk.data[0] = 42;
-        chunk.chunk.used += 1;
-        chunk.commit();
-
-        assert_eq!(* rx.get_chunks_for_tick().into_iter().next().unwrap(), 42 );
-        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
-    }
-
-    #[test]
-    fn test_dirty_write() {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let chunk = tx.borrow_chunk_mut().unwrap();
-        chunk.chunk.data[0] = 42;
-        chunk.chunk.used += 1;
-        
-        // Dont commit, cant read anzthing
-
-        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
-    }
-
-    #[test]
-    fn test_weird_api_stuff() {
-        assert_eq!( Hubs::new(&HubsInitializerU64{}).split().1.get_chunks_for_tick().into_iter().into_iter().into_iter().into_iter().next(), None);        
-    }
-
-    #[test]
-    fn test_basic_usage() {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let j = thread::spawn(move || {
-            for i in 0 .. 7{
-                match tx.borrow_chunk_mut(){
-                    Some(chunk) => {
-                        for k in 0 .. 9 {
-                            chunk.chunk.data[k] = i*k as u64;
-                            chunk.chunk.used += 1;
-                        }
-                        chunk.commit();
-                    }
-                    None => panic!("Must not be full")
-                };
-            }
-        });
-
-        let _ = j.join();
-
-        let mut iter = rx.get_chunks_for_tick().into_iter();
-        for i in 0 .. 7{
-            for k in 0 .. 9{
-                assert_eq!(*iter.next().unwrap(), i*k as u64);
-            }
-        } 
-        assert_eq!(iter.next(), None);        
-    }
-
-    #[test]
-    fn test_full() {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let j = thread::spawn(move || {
-            for i in 0 .. HUBS_SIZE{
-                match tx.borrow_chunk_mut(){
-                    Some(chunk) => {
-                        chunk.chunk.data[0] = i as u64;
-                        chunk.chunk.used += 1;
-                        chunk.commit();       
-                    }
-                    None => {
-                        return i;
-                    }
-                }
-            }
-            return 0 // overfull
-        });
-
-        let amt = j.join().unwrap();
-        assert_eq!(amt,HUBS_SIZE-1);
-
-        let mut iter = rx.get_chunks_for_tick().into_iter();
-        for i in 0 .. amt{
-                assert_eq!(*iter.next().unwrap(), i as u64);
-        } 
-        assert_eq!(iter.next(), None);            
-    }
-
-    #[test]
-    fn test_refill() {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
-
-        let total_msg = 1000;
-
-
-        let j = thread::spawn(move || {
-            for i in 0 .. total_msg{
-                loop{
-                    match tx.borrow_chunk_mut(){
-                        Some(chunk) => {
-                            // println!("writingg with {}", i);
-                            chunk.chunk.data[0] = i ;
-                            chunk.chunk.used = 1;
-                            chunk.commit();   
-                            break
-                        }
-                        None => {
-                            sleep(Duration::from_millis(10))
-                        }
-                    }
-                }
-
-            }
-            mpsc_tx.send(0u8);
-        });
-
-        sleep(Duration::from_millis(1));
-
-        let mut ctr = 0;
-
-        loop  {
-            for i in rx.get_chunks_for_tick().into_iter(){
-                assert_eq!(*i, ctr);
-                // println!("---------------------------------- Item val = {} ctr = {}",*i, ctr);
-                ctr +=1;
-            }
-            if mpsc_rx.try_recv().is_ok() {
-                // empty and other thread signals that they are done -> we are done too
-                break
-            }
-            sleep(Duration::from_millis(3))
-        }
-       
-        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
-
-        assert_eq!(ctr, total_msg);
-
-        j.join().unwrap();        
-    }
-
-    #[test]
-    fn test_stress_small(){
-        test_stress(      1);
-        test_stress(     10);
-        test_stress(    100);
-        test_stress(  1_000);
-        test_stress( 10_000);
-        test_stress(100_000);
-
-        test_stress(35_251);
-        test_stress(23_339);
-
-
-        test_stress(HUBS_SIZE  as u64 - 1);
-        test_stress(HUBS_SIZE  as u64    );
-        test_stress(HUBS_SIZE  as u64 + 1);
-
-        test_stress(HUBS_SIZE  as u64 / 2);
-        test_stress(HUBS_SIZE  as u64 * 2);
-
-        test_stress_multiple_data(    1,  1);
-        test_stress_multiple_data(   10, 10);
-        test_stress_multiple_data  (100,100);
-        test_stress_multiple_data(1_000, 10);
-
-        test_stress_multiple_data(1_000, 128);
-        test_stress_multiple_data(1_000, 127);
-
-    }
-
-    #[test]
-    #[ignore = "takes long"]
-    fn test_stress_large(){
-        test_stress( 10_000_000);
-    }
-    
-    fn test_stress(amt: u64) {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-
-        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
-
-        let total_msg = amt;
-
-        let j = thread::spawn(move || {
-            for i in 0 .. total_msg{
-                loop{
-                    match tx.borrow_chunk_mut(){
-                        Some(chunk) => {
-                            // println!("writingg with {}", i);
-                            chunk.chunk.data[0] = i ;
-                            chunk.chunk.used = 1;
-                            chunk.commit();   
-                            break
-                        }
-                        None => {
-                            // println!("Hubs was full at i = {}",i);
-                            sleep(Duration::from_nanos(10))
-                        }
-                    }
-                }
-            }
-            mpsc_tx.send(0u8).unwrap();
-        });
-
-        sleep(Duration::from_millis(1));
-
-        let mut ctr = 0;
-
-        let mut last_try=false;
-        while !last_try  {
-            if mpsc_rx.try_recv().is_ok() {
-                last_try = true;
-                sleep(Duration::from_millis(7))
-            }     
-            for i in rx.get_chunks_for_tick().into_iter(){
-                assert_eq!(*i, ctr);
-                ctr +=1;
-            }       
-            sleep(Duration::from_nanos(10))      
-        }
-       
-        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
-        assert_eq!(ctr, total_msg);
-        j.join().unwrap();       
-    }
-
-
-
-
-
-    fn test_stress_multiple_data(chunk_count: usize, data_count: usize) {
-        let hubs = Hubs::new(&HubsInitializerU64{});
-        let (mut tx,rx) = hubs.split();
-    
-        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
-    
-    
-        let j = thread::spawn(move || {
-            let mut ctr = 0;
-            for i in 0 .. chunk_count{
-                loop{
-                    match tx.borrow_chunk_mut(){
-                        Some(chunk) => {
-                            for k in 0 .. data_count{
-                                chunk.chunk.data[k] = ctr;
-                                ctr += 1;
-                            }
-                            chunk.chunk.used = data_count;
-                            chunk.commit();   
-                            break
-                        }
-                        None => {
-                            sleep(Duration::from_nanos(10))
-                        }
-                    }
-                }
-            }
-            mpsc_tx.send(0u8).unwrap();
-        });
-    
-        sleep(Duration::from_millis(1));
-    
-        let mut ctr = 0;
-    
-        let mut last_try=false;
-        while !last_try  {
-            if mpsc_rx.try_recv().is_ok() {
-                last_try = true;
-                sleep(Duration::from_millis(7))
-            }     
-            for i in rx.get_chunks_for_tick().into_iter(){
-                assert_eq!(*i, ctr);
-                ctr +=1;
-            }          
-            sleep(Duration::from_nanos(10))   
-        }
-       
-        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
-        assert_eq!(ctr as usize, chunk_count*data_count);
-        j.join().unwrap();       
     }
 }
---
+++ tests/hubs_test_basic.rs
@@ -0,0 +1,313 @@
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::{sync::mpsc, thread::{self, sleep}, time::Duration};
+
+    use hubs::{HUBS_SIZE, HubsInitializer};
+
+    use hubs::Hubs;
+    
+    struct HubsInitializerU64{}
+    impl HubsInitializer for HubsInitializerU64{
+        type T = u64;
+
+        fn initialize_data(&self)-> Self::T {
+            0
+        }
+    }
+
+    #[test]
+    fn test_write_single() {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let chunk = tx.borrow_chunk_mut().unwrap();
+        chunk.chunk.data[0] = 42;
+        chunk.chunk.used += 1;
+        chunk.commit();
+
+        assert_eq!(* rx.get_chunks_for_tick().into_iter().next().unwrap(), 42 );
+        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
+    }
+
+    #[test]
+    fn test_dirty_write() {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let chunk = tx.borrow_chunk_mut().unwrap();
+        chunk.chunk.data[0] = 42;
+        chunk.chunk.used += 1;
+        
+        // Dont commit, cant read anzthing
+
+        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
+    }
+
+    #[test]
+    fn test_weird_api_stuff() {
+        assert_eq!( Hubs::new(&HubsInitializerU64{}).split().1.get_chunks_for_tick().into_iter().into_iter().into_iter().into_iter().next(), None);        
+    }
+
+    #[test]
+    fn test_basic_usage() {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let j = thread::spawn(move || {
+            for i in 0 .. 7{
+                match tx.borrow_chunk_mut(){
+                    Some(chunk) => {
+                        for k in 0 .. 9 {
+                            chunk.chunk.data[k] = i*k as u64;
+                            chunk.chunk.used += 1;
+                        }
+                        chunk.commit();
+                    }
+                    None => panic!("Must not be full")
+                };
+            }
+        });
+
+        let _ = j.join();
+
+        let mut iter = rx.get_chunks_for_tick().into_iter();
+        for i in 0 .. 7{
+            for k in 0 .. 9{
+                assert_eq!(*iter.next().unwrap(), i*k as u64);
+            }
+        } 
+        assert_eq!(iter.next(), None);        
+    }
+
+    #[test]
+    fn test_full() {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let j = thread::spawn(move || {
+            for i in 0 .. HUBS_SIZE{
+                match tx.borrow_chunk_mut(){
+                    Some(chunk) => {
+                        chunk.chunk.data[0] = i as u64;
+                        chunk.chunk.used += 1;
+                        chunk.commit();       
+                    }
+                    None => {
+                        return i;
+                    }
+                }
+            }
+            return 0 // overfull
+        });
+
+        let amt = j.join().unwrap();
+        assert_eq!(amt,HUBS_SIZE-1);
+
+        let mut iter = rx.get_chunks_for_tick().into_iter();
+        for i in 0 .. amt{
+                assert_eq!(*iter.next().unwrap(), i as u64);
+        } 
+        assert_eq!(iter.next(), None);            
+    }
+
+    #[test]
+    fn test_refill() {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
+
+        let total_msg = 1000;
+
+
+        let j = thread::spawn(move || {
+            for i in 0 .. total_msg{
+                loop{
+                    match tx.borrow_chunk_mut(){
+                        Some(chunk) => {
+                            // println!("writingg with {}", i);
+                            chunk.chunk.data[0] = i ;
+                            chunk.chunk.used = 1;
+                            chunk.commit();   
+                            break
+                        }
+                        None => {
+                            sleep(Duration::from_millis(10))
+                        }
+                    }
+                }
+
+            }
+            mpsc_tx.send(0u8);
+        });
+
+        sleep(Duration::from_millis(1));
+
+        let mut ctr = 0;
+
+        loop  {
+            for i in rx.get_chunks_for_tick().into_iter(){
+                assert_eq!(*i, ctr);
+                // println!("---------------------------------- Item val = {} ctr = {}",*i, ctr);
+                ctr +=1;
+            }
+            if mpsc_rx.try_recv().is_ok() {
+                // empty and other thread signals that they are done -> we are done too
+                break
+            }
+            sleep(Duration::from_millis(3))
+        }
+       
+        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
+
+        assert_eq!(ctr, total_msg);
+
+        j.join().unwrap();        
+    }
+
+    #[test]
+    fn test_stress_small(){
+        test_stress(      1);
+        test_stress(     10);
+        test_stress(    100);
+        test_stress(  1_000);
+        test_stress( 10_000);
+        test_stress(100_000);
+
+        test_stress(35_251);
+        test_stress(23_339);
+
+
+        test_stress(HUBS_SIZE  as u64 - 1);
+        test_stress(HUBS_SIZE  as u64    );
+        test_stress(HUBS_SIZE  as u64 + 1);
+
+        test_stress(HUBS_SIZE  as u64 / 2);
+        test_stress(HUBS_SIZE  as u64 * 2);
+
+        test_stress_multiple_data(    1,  1);
+        test_stress_multiple_data(   10, 10);
+        test_stress_multiple_data(  100,100);
+        test_stress_multiple_data(1_000, 10);
+
+        test_stress_multiple_data(1_000, 128);
+        test_stress_multiple_data(1_000, 127);
+
+    }
+
+    #[test]
+    #[ignore = "takes long"]
+    fn test_stress_large(){
+        test_stress( 10_000_000);
+    }
+    
+    fn test_stress(amt: u64) {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+
+        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
+
+        let total_msg = amt;
+
+        let j = thread::spawn(move || {
+            for i in 0 .. total_msg{
+                loop{
+                    match tx.borrow_chunk_mut(){
+                        Some(chunk) => {
+                            // println!("writingg with {}", i);
+                            chunk.chunk.data[0] = i ;
+                            chunk.chunk.used = 1;
+                            chunk.commit();   
+                            break
+                        }
+                        None => {
+                            // println!("Hubs was full at i = {}",i);
+                            sleep(Duration::from_nanos(10))
+                        }
+                    }
+                }
+            }
+            mpsc_tx.send(0u8).unwrap();
+        });
+
+        sleep(Duration::from_millis(1));
+
+        let mut ctr = 0;
+
+        let mut last_try=false;
+        while !last_try  {
+            if mpsc_rx.try_recv().is_ok() {
+                last_try = true;
+                sleep(Duration::from_millis(7))
+            }     
+            for i in rx.get_chunks_for_tick().into_iter(){
+                assert_eq!(*i, ctr);
+                ctr +=1;
+            }       
+            sleep(Duration::from_nanos(10))      
+        }
+       
+        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
+        assert_eq!(ctr, total_msg);
+        j.join().unwrap();       
+    }
+
+
+
+
+
+    fn test_stress_multiple_data(chunk_count: usize, data_count: usize) {
+        let hubs = Hubs::new(&HubsInitializerU64{});
+        let (mut tx,rx) = hubs.split();
+    
+        let (mpsc_tx, mpsc_rx) =  mpsc::channel();
+    
+    
+        let j = thread::spawn(move || {
+            let mut ctr = 0;
+            for i in 0 .. chunk_count{
+                loop{
+                    match tx.borrow_chunk_mut(){
+                        Some(chunk) => {
+                            for k in 0 .. data_count{
+                                chunk.chunk.data[k] = ctr;
+                                ctr += 1;
+                            }
+                            chunk.chunk.used = data_count;
+                            chunk.commit();   
+                            break
+                        }
+                        None => {
+                            sleep(Duration::from_nanos(10))
+                        }
+                    }
+                }
+            }
+            mpsc_tx.send(0u8).unwrap();
+        });
+    
+        sleep(Duration::from_millis(1));
+    
+        let mut ctr = 0;
+    
+        let mut last_try=false;
+        while !last_try  {
+            if mpsc_rx.try_recv().is_ok() {
+                last_try = true;
+                sleep(Duration::from_millis(7))
+            }     
+            for i in rx.get_chunks_for_tick().into_iter(){
+                assert_eq!(*i, ctr);
+                ctr +=1;
+            }          
+            sleep(Duration::from_nanos(10))   
+        }
+       
+        assert_eq!( rx.get_chunks_for_tick().into_iter().next(), None);        
+        assert_eq!(ctr as usize, chunk_count*data_count);
+        j.join().unwrap();       
+    }
+}
